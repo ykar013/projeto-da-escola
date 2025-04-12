f6197df173b3cafecb0494ddefd9ee3f7cc43600
ykar013 <icarosilvaaaaa31@gmail.com>
2025-04-11 23:00:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCr1Q91nX6nJURSxCFTUf28K5O3RPNXCts",
+    authDomain: "projeto-da-escola-1a566.firebaseapp.com",
+    projectId: "projeto-da-escola-1a566",
+    storageBucket: "projeto-da-escola-1a566.firebasestorage.app",
+    messagingSenderId: "12575688268",
+    appId: "1:12575688268:web:cbd9703f25c1c7e02ed6e9"
+  };
